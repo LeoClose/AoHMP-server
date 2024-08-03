316314d4194b93a4d0f4b44792a591347239240c
LeoClose <leoclose575@gmail.com>
2024-08-03 18:11:09 +0200

    v2

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var app = require('express');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var http = require('http');
+
+http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+    resp.on('data', function(ip) {
+      console.log("My public IP address is: " + ip);
+    });
+});
 
 server.listen(8080, function(){
     console.log("[Server] Running");
